8159c06e474fa5ea9beb874576aacd3126663fe2
sonkyung <sonkyung@gmail.com>
2021-08-11 08:29:55 -0700

    update: Deque.java
    small updates in order to progress the auto-grader on Deque.java for feedback

---
+++ Week2/queues/src/Permutation.java
@@ -0,0 +1,15 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+
+public class Permutation {
+
+    public static void main(String[] args) {
+        Deque<String> dq = new Deque<String>();
+        dq.addFirst("A");
+
+    }
+}
