43ca96475c585eabce4e2d0cd691e701f5d031dc
sonkyung <sonkyung@gmail.com>
2022-02-11 11:29:25 -0800

    Update Permutation.java (week2)
    From Week2: Queues assignment submitted two weeks ago. Without this client, Coursera auto-grader passed at 83%, but I wanted to go back and implement the client. After this client implementation, assignment grade increased from 83% -> 94%!

--- Week2/queues/src/Permutation.java
+++ Week2/queues/src/Permutation.java
@@ -4,17 +4,34 @@
  *  Description:
  **************************************************************************** */
 
-import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdIn;
+import java.util.Iterator;
 
 
 public class Permutation {
 
     public static void main(String[] args) {
-        Deque<String> dq = new Deque<String>();
-        dq.addFirst("A");
+        RandomizedQueue<String> rq = new RandomizedQueue<>();
+        int n = 0;
 
-        int szz = 9;
-        System.out.println("random_n=" + StdRandom.uniform(szz) + " of [0," + szz + ")");
+        //read n strings from standard input
+        int k = Integer.parseInt(args[0]);
+
+
+        while(!StdIn.isEmpty()) {
+            String str = StdIn.readString();
+            rq.enqueue(str);
+            ++n;
+        }
+
+        if(k > n) throw new RuntimeException();
+
+        //print a k subset of strings (0 <= k <= n)
+        Iterator<String> itr = rq.iterator();
+        for(int i = 0; i < k; ++i) {
+            String str = itr.next();
+            System.out.println(str);
+        }
 
     }
 }
