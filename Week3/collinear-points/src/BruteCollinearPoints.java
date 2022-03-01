45c8715b5bab2050e4bbef7944bd6d3d087583ae
sonkyung <sonkyung@gmail.com>
2022-02-28 20:15:28 -0800

    Update BruteCollinearPoints.java
    taking a different approach.
    
    initial approach: enumerate a subset k of n (k < n), using binary representation of a number. this approach fails for n > 19. because a long is only about 19 binary digits long
    
    new approach: enumerate a subset k of n (k < n), using k-nested for loops. assert that a permutation is unique if each consecutive element is strictly increasing

--- Week3/collinear-points/src/BruteCollinearPoints.java
+++ Week3/collinear-points/src/BruteCollinearPoints.java
@@ -1,7 +1,7 @@
 
-import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdDraw;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -15,96 +15,60 @@ public class BruteCollinearPoints {
         lineSegs = new ArrayList<LineSegment>();
         if (points == null) throw new IllegalArgumentException();
 
-        //check if any point is null
+
         for(int i = 0; i < n; ++i) {
+            //check if any point is null
             if (points[i] == null) throw new IllegalArgumentException();
-        }
 
-        //sort the points based on their (x,y)-coordinate
-        ArrayList<Point> all_pts = new ArrayList<>(Arrays.asList(points));
-        all_pts.sort(Point::compareTo);
-
-        //check for any repeated point
-        for(int i = 1; i < n; ++i) {
-            if (points[i].slopeTo(points[i-1]) == Double.NEGATIVE_INFINITY) throw new IllegalArgumentException();
-        }
-
-
-        //consider all subsets of 4 points -- because at least 4 points must be collinear
-        if(n >= 4) {
-            // 1. create a list of all 4-point subsets
-            long N = Long.parseLong("1111",2) * (long)Math.pow(2,4);     // equals 15 * 2^4 (shift to left 4 binary digits)
-            for(long perm_id = 15; perm_id <= N; ++perm_id) {     //starts at perm_id=15, b/c 15 = 0001111 = 15 (first subset consisting of first 4 points)
-
-                // 2. iterate through that list, one 4-point subset at a time
-                if (is4pointSubset(perm_id, n)) {
-                    ArrayList<Point> pts = GetPointsSubset(perm_id, points);
-                    //System.out.println("pts=" + pts);
-                    pts.sort(Point::compareTo);
-                    //System.out.println("pts=" + pts);
-
-                    // 3. check if that chosen 4-point subset is collinear: [ p0 <=> p1 <=> p2 <=> p3 ]
-                    boolean isCollinear = true;
-                    isCollinear = isCollinear && (pts.get(1).slopeOrder().compare(pts.get(0), pts.get(2)) == 0);
-                    isCollinear = isCollinear && (pts.get(2).slopeOrder().compare(pts.get(1), pts.get(3)) == 0);
-
-                    if (isCollinear) {
-                        LineSegment ls = new LineSegment(pts.get(0), pts.get(3));
-                        lineSegs.add(ls);
-
-                        //System.out.println("perm_id=" + perm_id + ", binStr=" + getBinaryPermString(perm_id, n) + ", isCollinear=" + isCollinear);
-                    }
-
-                    //System.out.println("perm_id=" + perm_id + ", isCollinear=" + isCollinear);
+            //check for any repeated point
+            for(int j = 1; j < n; ++j) {
+                if(i < j) {
+                    if (points[i].slopeTo(points[j]) == Double.NEGATIVE_INFINITY)
+                        throw new IllegalArgumentException();
                 }
             }
         }
 
 
+        //consider all subsets of 4 points -- because at least 4 points must be collinear
+        if(n >= 4) {
 
-    }
+            for (int i = 0; i < n; ++i) {
+                for (int j = 1; j < n; ++j) {
+                    for (int k = 2; k < n; ++k) {
+                        for (int l = 3; l < n; ++l) {
 
-    private static String getBinaryPermString(long perm_id, long max_n) {
-        String binStr = "000000000000000" + Long.toBinaryString(perm_id);
-        binStr = binStr.substring(binStr.length() - (int)max_n, binStr.length());
+                            // condition(i < j < k < l) => unique 4 point subset permutation
+                            if ((i < j) && (j < k) && (k < l)) {
+                                ArrayList<Point> pts = new ArrayList<Point>();
+                                pts.add(points[i]);
+                                pts.add(points[j]);
+                                pts.add(points[k]);
+                                pts.add(points[l]);
 
-        return binStr;
-    }
+                                pts.sort(Point::compareTo);       // needed??
 
-    private static boolean is4pointSubset(long perm_id, long max_n) {
-        boolean is4points = false;
-        int cnt = 0;
+                                // check if that chosen 4-point subset is collinear: [ p0 <=> p1 <=> p2 <=> p3 ]
+                                boolean isCollinear = true;
+                                isCollinear = isCollinear && (pts.get(1).slopeOrder().compare(pts.get(0), pts.get(2)) == 0);
+                                isCollinear = isCollinear && (pts.get(2).slopeOrder().compare(pts.get(1), pts.get(3)) == 0);
 
-        String binStr = getBinaryPermString(perm_id, max_n);
-        char[] chrArr = binStr.toCharArray();
+                                if (isCollinear) {
+                                    LineSegment ls = new LineSegment(pts.get(0), pts.get(3));
+                                    lineSegs.add(ls);
+                                }
 
-        for (int i = 0; i < chrArr.length; i++) {
-            if(chrArr[i] == '1') ++cnt;
-        }
 
-        return (cnt == 4);
-    }
+                            }
 
-    private static ArrayList<Point> GetPointsSubset(long perm_id, Point[] points) {
-        ArrayList<Point> pts = new ArrayList<>(Arrays.asList(new Point[4]));
-        String pad0 = "0";
-        for (int i = 0; i < points.length; i++) {
-            pad0 = pad0 + "0";
-        }
-        String binStr = pad0;
-        binStr = binStr + Long.toBinaryString(perm_id);
-        binStr = binStr.substring(binStr.length()-points.length,binStr.length());
-        char[] chrArr = binStr.toCharArray();
-        assert chrArr.length == points.length;
 
-        int cnt = 0;
-        for (int i = 0; i < points.length; i++) {
-            if(chrArr[i] == '1') pts.set(cnt++, points[i]);
-        }
+                        }   // l
+                    }   // k
+                }   // j
+            }   // i
 
-        //System.out.println("perm_id=" + perm_id + ", " + binStr);
+        }   // if n>=4
 
-        return pts;
     }
 
 
@@ -133,8 +97,8 @@ public class BruteCollinearPoints {
         }
         // draw the points
         StdDraw.enableDoubleBuffering();
-        StdDraw.setXscale(0, 32768);
-        StdDraw.setYscale(0, 32768);
+        StdDraw.setXscale(-100, 32768);
+        StdDraw.setYscale(-100, 32768);
         StdDraw.setPenColor(0, 0, 0);
         StdDraw.setPenRadius(0.01);
         for (Point p : points) {
@@ -144,7 +108,7 @@ public class BruteCollinearPoints {
 
 
         // print and draw the line segments
-        StdDraw.setPenRadius(0.001);
+        StdDraw.setPenRadius(0.002);
         StdDraw.setPenColor(255, 0, 0);
         BruteCollinearPoints collinear = new BruteCollinearPoints(points);
         for (LineSegment segment : collinear.segments()) {
